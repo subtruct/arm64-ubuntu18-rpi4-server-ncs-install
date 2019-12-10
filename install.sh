0359a3e762604542d0767fb7dd13060bb490844f
subtruct <47941859+subtruct@users.noreply.github.com>
2019-12-11 01:44:37 +0200

    Fix force to YES

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ alias python=python3
 alias pip=pip3
 
 # virtualenv and virtualenvwrapper
-sudo pip install ncs virtualenvwrapper
+sudo yes | pip -q install ncs virtualenvwrapper
 sudo rm -rf ~/get-pip.py ~/.cache/pip
 export WORKON_HOME=$HOME/.ncs
 export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
@@ -21,18 +21,18 @@ echo "workon ncs" >> ~/.bashrc
 source ~/.bashrc
 
 # python 3.7
-sudo apt-get install python3.7-dev
+sudo apt-get -y install python3.7-dev
 update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 1
 workon cv
 #libs & tools
 sudo apt-get -y install build-essential cmake unzip pkg-config
-sudo apt-get install libjpeg-dev libpng-dev libtiff-dev
-sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
-sudo apt-get install libxvidcore-dev libx264-dev
-sudo apt-get install libcanberra-gtk*
-sudo apt-get install libatlas-base-dev gfortran
+sudo apt-get -y install libjpeg-dev libpng-dev libtiff-dev
+sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get -y install libxvidcore-dev libx264-dev
+sudo apt-get -y install libcanberra-gtk*
+sudo apt-get -y install libatlas-base-dev gfortran
 
-pip install numpy
+pip -q install numpy==
 
 
 
