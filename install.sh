e57d39dbee681c4481373ab22d2da0b603d6fb0f
subtruct <47941859+subtruct@users.noreply.github.com>
2019-12-13 21:57:36 +0200

    BETA TEST - no env

--- install.sh
+++ install.sh
@@ -5,15 +5,15 @@ sudo apt -y install wget mc python python3 python-pip python3-pip python3.6-dev
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 1
 wget https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py
-pip3 install virtualenv virtualenvwrapper
+#pip3 install virtualenv virtualenvwrapper
 # virtualenv and virtualenvwrapper
-echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
-echo -e "\n# virtualenv and virtualenvwrapper" >> ~/.bashrc
-echo "export WORKON_HOME=$HOME/.venvs" >> ~/.bashrc
-echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3.6" >> ~/.bashrc
+#echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
+#echo -e "\n# virtualenv and virtualenvwrapper" >> ~/.bashrc
+#echo "export WORKON_HOME=$HOME/.venvs" >> ~/.bashrc
+#echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3.6" >> ~/.bashrc
 #echo "export OpenCV_DIR=/usr/local/opencv4" >> ~/.bashrc
-echo "export PATH=/home/ubuntu/.local/bin:/home/ubuntu/.virtualenvs/cv/bin"
-echo "source $WORKON_HOME/cv/bin/activate" >> ~/.bashrc
+#echo "export PATH=/home/ubuntu/.local/bin:/home/ubuntu/.virtualenvs/cv/bin"
+#echo "source $WORKON_HOME/cv/bin/activate" >> ~/.bashrc
 echo "alias python=python3.6" >> ~/.bashrc
 echo "alias pip=pip3" >> ~/.bashrc
 source ~/.bashrc
@@ -32,8 +32,6 @@ sudo pip3 install numpy && sudo pip3 install scikit-image
 sudo pip3 install google #caffe dependences 
 sudo pip3 install protobuff pyyaml
 
-
-
 #Custom NCS SDK/API Installation
 #cd && one https://github.com/markjay4k/ncsdk-aarch64.git
 #git clone https://github.com/subtruct/arm64-ubuntu18-rpi4-server-ncs-install.git
@@ -43,32 +41,13 @@ sudo pip3 install protobuff pyyaml
 #sudo make install
 #sudo make api3##
 
-#--OPEN-CV Installation
-cd && git clone https://github.com/opencv/opencv.git
-git clone https://github.com/opencv/opencv_contrib.git
-cd ~/opencv
-mkdir build && cd build 
-cmake -D CMAKE_BUILD_TYPE=RELEASE \
-    -D CMAKE_INSTALL_PREFIX=/usr/local \
-    -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
-    -D ENABLE_NEON=ON \
-    -D ENABLE_VFPV3=ON \
-    -D BUILD_TESTS=OFF \
-    -D OPENCV_ENABLE_NONFREE=ON \
-    -D INSTALL_PYTHON_EXAMPLES=ON \
-    -D BUILD_EXAMPLES=ON \
-    -D BUILD_opencv_python2=False \
-    -D BUILD_opencv_python3=True \
-    -D PYTHON_DEFAULT_EXECUTABLE=python3 \
-    -D PYTHON_EXECUTABLE=python3 \
-make -j4
-sudo make install
-
 #--NCS SDK/API on Ubuntu18.1. Build.2019.3.376
 #cd && wget https://download.01.org/opencv/2019/openvinotoolkit/R3/l_openvino_toolkit_dev_ubuntu18_p_2019.3.376.tgz
 #tar -xvzf l_openvino_toolkit_dev_ubuntu18_p_2019.3.376.tgz
-
-#mv ./l_openvino_toolkit_dev_ubuntu18_p_2019.3.376 ./dldt
+cd && wget https://download.01.org/opencv/2019/openvinotoolkit/R3/l_openvino_toolkit_runtime_ubuntu18_p_2019.3.376.tgz
+tar -xf l_openvino_toolkit_runtime_ubuntu18_p_2019.3.376.tgz
+#sudo cp -R ./l_openvino_toolkit_runtime_raspbian_p_2019.2.242 /srv/chroot/buster_armhf/
+mv ./l_openvino_toolkit_runtime_ubuntu18_p_2019.3.376 ./dldt
 cd ~/dldt/inference-engine
 git submodule init
 git submodule update --recursive
@@ -79,18 +58,46 @@ sudo make install
 ln -s /usr/local/lib/python3.6/dist-packages/mvnc .
 ln -s /usr/local/lib/python3.6/dist-packages/graphviz .
 
+
+#--OPEN-CV Installation
+cd && git clone https://github.com/opencv/opencv.git
+git clone https://github.com/opencv/opencv_contrib.git
+cd ~/opencv
+mkdir build && cd build 
+cmake -D CMAKE_INSTALL_PREFIX=/usr/local \
+      -D PYTHON3_EXECUTABLE=/usr/bin/python3.6 \
+      -D PYTHON3_INCLUDE_DIR=/usr/include/python3.6m \
+      -D PYTHON3_PACKAGES_PATH=/usr/lib/python3.6/dist-packages \
+      -D PYTHON_DEFAULT_EXECUTABLE=/usr/bin/python3.6 \
+      -D BUILD_OPENCV_PYTHON3=yes \
+      -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+      -D CMAKE_BUILD_TYPE=Release \
+      -D WITH_IPP=OFF \
+      -D BUILD_TESTS=OFF \
+      -D BUILD_PERF_TESTS=OFF \
+      -D BUILD_EXAMPLES=OFF \
+      -D ENABLE_PRECOMPILED_HEADERS=OFF \
+      -D ENABLE_NEON=ON \
+      -D WITH_INF_ENGINE=ON \
+      -D INF_ENGINE_LIB_DIRS="~/dldt/deployment_tools/inference_engine/lib/armv7l" \
+      -D INF_ENGINE_INCLUDE_DIRS="~/dldt/deployment_tools/inference_engine/include" \
+      -D CMAKE_FIND_ROOT_PATH="~/dldt/" \
+      -D ENABLE_CXX11=ON ..
+make -j4
+sudo make install
+
 sudo usermod -a -G users "$(whoami)"
 
-cd && source ./dldt/bin/setupvars.sh
-sh ./dldt/install_dependencies/install_NCS_udev_rules.sh
+#cd && source ./dldt/bin/setupvars.sh
+#sh ./dldt/install_dependencies/install_NCS_udev_rules.sh
 
 
 #-- Link to source files  
-cd ~/.virtualenvs/cv/lib/python3.6/site-packages/
-sudo mv cv2.so cv2.so.bak
-sudo ln -s /usr/local/lib/python3.6/site-packages/cv2.cpython-32mu.so cv2.so
+#cd ~/.virtualenvs/cv/lib/python3.6/site-packages/
+#sudo mv cv2.so cv2.so.bak
+#sudo ln -s /usr/local/lib/python3.6/site-packages/cv2.cpython-32mu.so cv2.so
 
 #--Tensorflow silent installation
-cd && git clone https://github.com/markjay4k/Tensorflow-1.9rc0-py36-aarch64.git
-sudo apt install Tensorflow-1.9rc0-py36-aarch64/tensorflow-1.9.0rc0-cp36-cp36m-linux_aarch64.whl
-sudo reboot 
+#cd && git clone https://github.com/markjay4k/Tensorflow-1.9rc0-py36-aarch64.git
+#sudo apt install Tensorflow-1.9rc0-py36-aarch64/tensorflow-1.9.0rc0-cp36-cp36m-linux_aarch64.whl
+#sudo reboot 
